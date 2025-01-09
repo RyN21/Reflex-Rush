968be2f6f1f682247e07420456c93d5886cb24cf
Ryan Laleh <33795357RyN21@users.noreply.github.com>
2025-01-09 12:39:19 -0700

    Created state_manager and main menu files to create a main menu screen and to be able to switch between states of the game

--- main.rb
+++ main.rb
@@ -2,5 +2,47 @@ Dir[File.join(File.dirname(__FILE__), "lib", "**.rb")].each do |file|
   require file
 end
 
+
+
+# Entry Point for game
+
+require "gosu"
+require_relative "lib/state_manager"
+require_relative "lib/main_menu"
+require_relative "lib/game_window"
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT= 600
+FLOOR        = 525
+TITLE        = "Reflex Rush"
+
+class Main < Gosu::Window
+  def initialize
+    super WINDOW_WIDTH, WINDOW_HEIGHT
+    self.caption = TITLE
+
+    @state_manager = StateManger.new(self)
+  end
+
+  def update
+    @state_manager.update
+  end
+
+  def draw
+    @state_manager.draw
+  end
+
+  def button_down id
+    @state_manager.button_down id
+  end
+end
+
+Maoin.new.show
+
+
+
+
+
+
 # Game.setup
-WindowGame.start
+# WindowGame.start
