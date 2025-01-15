86d0d3fb0b4c9a3ff74fe31642390abf29c6ab4b
Ryan Laleh <33795357+RyN21@users.noreply.github.com>
2025-01-15 12:01:22 -0700

    Add button to start game from main menu, 'Press ENTER to Start'

--- lib/main_menu.rb
+++ lib/main_menu.rb
@@ -15,9 +15,12 @@ class MainMenu
   def initialize state_manager
     @state_manager = state_manager
     @background    = Gosu::Image.new("graphics/background_14.png")
-    @logo          = Gosu::Image.new("graphics/logo_bg.png")
+    @logo          = Gosu::Image.new("graphics/logo.png")
     @font          = Gosu::Font.new(30)
     @menu_music    = Gosu::Song.new("sounds/credits_music.mp3")
+    @start_box     = Gosu::Image.new("graphics/score_background.png")
+    @start_button_x      = 260
+    @start_button_y      = 500
     @difficulty    = []
     @menu_music.play(true)
   end
@@ -26,17 +29,18 @@ class MainMenu
 
   def draw
     @background.draw -21, 0, 0, 1.35, 1.35
-    @logo.draw 183, -28, 0, 0.55, 0.55
-    # @font
+    @logo.draw 175, -28, 0, 0.55, 0.55
+    @start_box.draw @start_button_x, @start_button_y, 0, 0.3, 0.103
+    @font.draw_text("Press ENTER to Start", @start_button_x + 15, @start_button_y + 3, 2, 1, 1)
   end
 
   def button_down id
     case id
+    when Gosu::KB_ESCAPE
+      exit
     when Gosu::KB_RETURN
       @menu_music.stop
       @state_manager.switch_to(GameWindow.new(@state_manager))
-    when Gosu::KB_ESCAPE
-      exit
     end
   end
 end
