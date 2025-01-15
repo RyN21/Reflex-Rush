06e19a07450e6a495d943456415659ba5fbffc9e
Ryan Laleh <33795357+RyN21@users.noreply.github.com>
2025-01-15 12:01:21 -0700

    commit before switching to new branch

--- lib/game.rb
+++ lib/game.rb
@@ -136,7 +136,7 @@ end
 class Rock
   attr_reader :x, :y
   def initialize(difficulty)
-    rock_image = Gosu::Image.new("graphics/rock.png")
+    rock_image  = Gosu::Image.new("graphics/rock.png")
     @rock       = rock_image.subimage(148,170,300,262)
     @floor      = FLOOR + @rock.height * 0.15
     @x          = rand 0..800
@@ -185,12 +185,12 @@ class Rock
   # end
 end
 
-class PowerUp1
+class PowerUp
+  attr_reader :x, :y
+
   def initialize
-    pu1_image   = Gosu::Image.new("graphics/ruby.png")
-    pu2_image   = Gosu::Image.new("graphics/coin.png")
-    @power_up_2 = pu1_image.subimage(148,170,300,262)
-    @power_up_2 = pu2_image.subimage(148,170,300,262)
+    pu1_image  = Gosu::Image.new("graphics/ruby.png")
+    @power_up_1 = pu1_image.subimage(148,170,300,262)
     @x          = 300
     @y          = 50
     @vel        = rand 3..7
@@ -199,7 +199,7 @@ class PowerUp1
   end
 
   def draw
-    @@power_up_1.draw_rot(@x,@y,0,  25 * Math.sin(Gosu.milliseconds / 133.7), center_x = 0.5, center_y = 0.5, scale_x = 0.15, scale_y = 0.15)
+    @power_up_1.draw_rot(@x,@y,0,  5, center_x = 0.5, center_y = 0.5, scale_x = 0.25, scale_y = 0.25)
   end
 
   def update_power_up
@@ -211,29 +211,6 @@ class PowerUp1
     Gosu.draw_line(x - @power_up_1.width/2 * 0.15 + w, y - @power_up_1.height/2 *0.15, Gosu::Color::RED, x - @power_up_1.width/2 * 0.15 + w, y - @power_up_1.height/2 *0.15 + h, Gosu::Color::RED, 1) #right border
     Gosu.draw_line(x - @power_up_1.width/2 * 0.15 + w, y - @power_up_1.height/2 *0.15 + h, Gosu::Color::RED, x - @power_up_1.width/2 * 0.15, y - @power_up_1.height/2 *0.15 + h, Gosu::Color::RED, 1) #bottom border
     Gosu.draw_line(x - @power_up_1.width/2 * 0.15, y - @power_up_1.height/2 *0.15 + h, Gosu::Color::RED, x - @power_up_1.width/2 * 0.15, y - @power_up_1.height/2 *0.15, Gosu::Color::RED, 1) #left border
-  end
-end
-
-class PowerUp2
-  def initialize
-    pu2_image   = Gosu::Image.new("graphics/coin.png")
-    @power_up_2 = pu2_image.subimage(148,170,300,262)
-    @x          = rand 0..800
-    @y          = 0
-    @vel        = rand 0..7
-    @width      =
-    @height     =
-  end
-
-  def draw
-    @@power_up_2.draw_rot(@x,@y,0,  25 * Math.sin(Gosu.milliseconds / 133.7), center_x = 0.5, center_y = 0.5, scale_x = 0.15, scale_y = 0.15)
-  end
-
-  def draw_border(x, y, w, h)
-    Gosu.draw_line(x - @power_up_2.width/2 * 0.15, y - @power_up_2.height/2 *0.15, Gosu::Color::RED, x - @power_up_2.width/2 *0.15 + w, y - @power_up_2.height/2 *0.15, Gosu::Color::RED, 1) #top border
-    Gosu.draw_line(x - @power_up_2.width/2 * 0.15 + w, y - @power_up_2.height/2 *0.15, Gosu::Color::RED, x - @power_up_2.width/2 * 0.15 + w, y - @power_up_2.height/2 *0.15 + h, Gosu::Color::RED, 1) #right border
-    Gosu.draw_line(x - @power_up_2.width/2 * 0.15 + w, y - @power_up_2.height/2 *0.15 + h, Gosu::Color::RED, x - @power_up_2.width/2 * 0.15, y - @power_up_2.height/2 *0.15 + h, Gosu::Color::RED, 1) #bottom border
-    Gosu.draw_line(x - @power_up_2.width/2 * 0.15, y - @power_up_2.height/2 *0.15 + h, Gosu::Color::RED, x - @power_up_2.width/2 * 0.15, y - @power_up_2.height/2 *0.15, Gosu::Color::RED, 1) #left border
   end
 end
 
@@ -263,12 +240,11 @@ class Game < Gosu::Window
       @score_to_advance = 60
       @player           = Player.new
       @rocks            = []
-      @power_ups        = []
       @last_rock        = Gosu.milliseconds
-      @last_power_up    = Gosu.milliseconds
       @font             = Gosu::Font.new(20)
       @score_background = Gosu::Image.new("graphics/score_background.png")
       @logo_bg          = Gosu::Image.new("graphics/logo_bg.png")
+      @power_ups        = []
 
       @level_up_sound   = Gosu::Sample.new("sounds/level_up_2.mp3")
       @lose_sound       = Gosu::Sample.new("sounds/failed.mp3")
@@ -317,10 +293,7 @@ class Game < Gosu::Window
       @rocks.each do |rock|
         rock.draw
       end
-      if Gosu.milliseconds - @last_power_up > 2_000
-        @power_ups << PowerUp1.new
-        @last_power_up = Gosu.milliseconds
-      end
+
       @score_background.draw 0, 550, 0, 1, 0.19, 0xBFffffff
       @font.draw_text("Score: #{@player.score}",130,565, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
       @font.draw_text("Level: #{@level}/#{@backgrounds.length-1}", 20, 565, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
